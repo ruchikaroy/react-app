9bdadafa544b4ca1e6a945ecc14da1075302be85
ruchikaroy <contactruchikaroy@gmail.com>
2024-08-14 15:14:57 +0100

    This commit adds dynamic react-icon

---
+++ src/components/Like.tsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
+
+interface Props {
+  onClick: () => void;
+}
+
+const Like = ({ onClick }: Props) => {
+  const [status, setStatus] = useState(false);
+
+  const toggle = () => {
+    setStatus(!status);
+    onClick();
+  };
+
+  if (status) return <AiFillHeart size={40} color="#ffb09c" onClick={toggle} />;
+  return <AiOutlineHeart size={40} onClick={toggle} />;
+};
+
+export default Like;
